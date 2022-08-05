10fdcc83bd9b790a0cd1ec125661494420da19ed
kunwu <wk707060335@gmail.com>
2022-08-05 18:04:32 +0200

    fix

--- run_client.sh
+++ run_client.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-for BATCH_SIZE in 1 5 10 15 25 50 75 100 500 1000 5000 10000; do
+for BATCH_SIZE in 1 5 10 15 25 50 75 100 500 1000 5000 10000 20000 50000 100000; do
   # run a fixed time
-  ./target/release/client datasets/records.txt 1 $BATCH_SIZE 60
+  ./target/release/client datasets/queries.txt 1 $BATCH_SIZE 60
 done
 
